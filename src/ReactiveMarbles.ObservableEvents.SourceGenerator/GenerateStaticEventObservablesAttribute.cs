e250a080ba95c5eb6eca7612e4bd2e0be20837b6
Dan Friedman <dan@thefriedmans.org>
2022-08-26 22:58:53 -0500

    Pull static code out of generator

---
+++ src/ReactiveMarbles.ObservableEvents.SourceGenerator/GenerateStaticEventObservablesAttribute.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2019-2021 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+
+namespace ReactiveMarbles.ObservableEvents
+{
+    /// <summary>
+    /// Generates a IObservable`T` wrapper for the specified type.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class GenerateStaticEventObservablesAttribute : Attribute
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GenerateStaticEventObservablesAttribute"/> class.
+        /// </summary>
+        /// <param name="type">The static type to generate event observable wrappers for.</param>
+        public GenerateStaticEventObservablesAttribute(Type type)
+        {
+            Type = type;
+        }
+
+        /// <summary>Gets the Type to generate the static event observable wrappers for.</summary>
+        public Type Type { get; }
+    }
+}
---
+++ src/ReactiveMarbles.ObservableEvents.SourceGenerator/ObservableGeneratorExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2019-2021 ReactiveUI Association Incorporated. All rights reserved.
+// ReactiveUI Association Incorporated licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using System;
+
+namespace ReactiveMarbles.ObservableEvents
+{
+    /// <summary>
+    /// Extension methods to generate IObservable for contained events on the class.
+    /// </summary>
+    public static partial class ObservableGeneratorExtensions
+    {
+        /// <summary>
+        /// Gets observable wrappers for all the events contained within the class.
+        /// </summary>
+        /// <typeparam name="T">foo.</typeparam>
+        /// <param name="eventHost">foo bar.</param>
+        /// <returns>The events if available.</returns>
+        public static NullEvents Events<T>(this T eventHost)
+        {
+            return default;
+        }
+    }
+}
